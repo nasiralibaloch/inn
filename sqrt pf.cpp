8a4be84a27c3872a3f9af69870cc9443a9527a12
nasiralibaloch <70817789+nasiralibaloch@users.noreply.github.com>
2020-10-23 22:14:24 +0500

    Add files via upload

---
+++ sqrt pf.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	cout<<"welcom to chakwal"<<endl;
+  /*int a;
+   cout<<"enter the value a"<<endl;
+   cin>>a;
+   cout<<"sq root of"<<a<<"is ="<<sqrt(a)<<endl;
+   return 0;*/
+   
+}
+
+
